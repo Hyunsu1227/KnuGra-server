ba7e7777f28b4ffbec944d573d97e4f21b8a5911
Ubuntu <ubuntu@ip-172-31-37-54.ap-northeast-2.compute.internal>
2020-01-23 04:30:35 +0000

    add test_client_get_grade_info.py

---
+++ test_client_get_grade_info.py
@@ -0,0 +1,19 @@
+import argparse
+import json
+import socket
+import threading
+
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSock.connect(('127.0.0.1', 3456))
+
+print('연결 확인 됐습니다.')
+
+dic = {'requestType':"getGradeInfo","id":""}
+jsonstr = json.dumps(dic)
+print(jsonstr)
+clientSock.send(jsonstr.encode())
+
+print('메시지를 전송했습니다.')
+
+data = clientSock.recv(4096)
+print('받은 데이터 : ', data, type(data))
